892d1f17d9d6e5c4d9e07b260a128a7ac015beb5
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-03 12:05:53 +0100

    Add support for factorials

---
+++ src/fact.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# != 1 ] || ! [[ $1 =~ ^[0-9]+$ ]]; then
+	echo "Usage: fact <non_negative_integer>"
+	exit
+fi
+
+if [ $1 == 0 ]; then
+	echo -n 1
+	exit
+fi
+
+seq -s "*" 1 $1 | bc | tr -dc "0-9"
