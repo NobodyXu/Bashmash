844688148f0b7be997e3a355abd03d6fa79911ca
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-08 23:01:18 +0100

    Update the installer

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ cppDestinations=("/bin/fact" "/bin/dec2bin" "/bin/bin2dec")
 cppLibraries=("src/NonNegativeInteger.cpp" "src/NonNegativeInteger.cpp" "src/NonNegativeBinaryInteger.cpp")
 cppFiles=${#cppSources[@]}
 
+echo "Installing $name $version..."
+error="false"
+
 for (( i = 0; i < $cppFiles; i++ )); do
 	source="${cppSources[$i]}"
 	destination="${cppDestinations[$i]}"
@@ -18,6 +21,11 @@ for (( i = 0; i < $cppFiles; i++ )); do
 	echo -n "$source..."
 	g++ "$source" "$library" "-Isrc" -o "$destination"
 	
+	if [ $? != 0 ]; then
+		error="true"
+		continue
+	fi
+	
 	if [ $? == 0 ]; then
 		chmod +x "$destination"
 		if [ $? == 0 ]; then
@@ -25,3 +33,12 @@ for (( i = 0; i < $cppFiles; i++ )); do
 		fi
 	fi
 done
+
+if [ "$error" == "true" ]; then
+	echo "Errors have occurred while installing $name $version."
+	echo "Make sure that you've installed G++ and that you're installing as root."
+	
+	exit
+fi
+
+echo "$name $version has been successfully installed."
