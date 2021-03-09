823642cc4a2cb6f0efd691ea9bb702c74b3c73a1
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-09 14:53:39 +0100

    Add a decimal to octal converter

---
+++ src/dec2oct.cpp
@@ -0,0 +1,36 @@
+#include "NonNegativeInteger.h"
+
+#include <iostream>
+#include <sstream>
+
+void printUsage() {
+	cout << "Usage: dec2oct <non_negative_integer>" << endl;
+}
+
+string dec2oct(NonNegativeInteger* x) {
+	if (x->equalTo(0))
+		return "0";
+		
+	ostringstream y;
+	y << oct << x->toUnsignedLongLong();
+	
+	return y.str();
+}
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		printUsage();
+		return 1;
+	}
+	
+	NonNegativeInteger* x = new NonNegativeInteger(argv[1]);
+	if (!x->isValid()) {
+		printUsage();
+		return 1;
+	}
+	
+	string y = dec2oct(x);
+	cout << y;
+	
+	return 0;
+}
