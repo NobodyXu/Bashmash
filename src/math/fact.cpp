6978dd56f6fb55296b8196e364c04594c7122acf
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-13 23:02:43 +0100

    Move mathematical operations to a "math" folder

---
+++ src/math/fact.cpp
@@ -0,0 +1,12 @@
+#include "fact.h"
+
+NonNegativeInteger* fact(NonNegativeInteger* x) {
+	if (x->equalTo(0))
+		return new NonNegativeInteger(1);
+		
+	NonNegativeInteger* y = new NonNegativeInteger(1);
+	for (NonNegativeInteger* i = new NonNegativeInteger(2); i->lessThanOrEqualTo(x); i->increment())
+		y->multiply(i);
+		
+	return y;
+}
---
+++ src/math/fact.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "../integers/NonNegativeInteger.h"
+
+NonNegativeInteger* fact(NonNegativeInteger* x);
