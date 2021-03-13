601366fa54ee74ad591c80df7aae44f6a1c7b0ed
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-13 22:56:06 +0100

    Move the integer classes to an "integer" folder

---
+++ src/integers/NonNegativeInteger.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class NonNegativeInteger {
+private:
+	bool valid;
+
+	unsigned long long fieldCount;
+	unsigned long long* fields;
+	
+	bool isNonNegativeInteger(string x);
+	
+public:
+	NonNegativeInteger(unsigned long long x);
+	NonNegativeInteger(string x);
+	
+	unsigned long long toUnsignedLongLong();
+	string toString();
+	
+	
+	bool isValid();
+	
+	bool lessThanOrEqualTo(NonNegativeInteger* x);
+	bool equalTo(unsigned long long x);
+	
+	void increment();
+	void multiply(NonNegativeInteger* x);
+};
