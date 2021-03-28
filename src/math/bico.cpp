c075f75decee9e6a6b19cd6959b17d1aac6ef9f7
Hubert Pastyrzak <76949230+HubertPastyrzak@users.noreply.github.com>
2021-03-29 00:32:18 +0200

    Add comments for readability

--- src/math/bico.cpp
+++ src/math/bico.cpp
@@ -2,15 +2,18 @@
 #include "fact.h"
 
 NonNegativeInteger* bico(NonNegativeInteger* n, NonNegativeInteger* k) {
+	//z = n - k
 	NonNegativeInteger* z = new NonNegativeInteger(n);
 	z->subtract(k);
 	
+	//w = k! * z!
 	NonNegativeInteger* w = fact(k);
 	w->multiply(fact(z));
 	
 	if (w->equalTo(0))
 		throw exception();
 	
+	//y = n! / w
 	NonNegativeInteger* y = fact(n);
 	y->divide(w);
 	
